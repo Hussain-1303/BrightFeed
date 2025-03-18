0706561504ea1867cb9b0b807965b2d85e872d08
Hussain Vahora <101166530+Hussain-1303@users.noreply.github.com>
2025-03-18 10:28:44 -0400

    Fixing hierchy
    Fixed the Hierchy

--- brightfeed_temp/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}", // Make sure this is correct for your file paths
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
